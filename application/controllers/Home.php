2afc70f71529d6f1a33387623b870bb2d4777bb8
fuxiaochun <f2e.xiaochun@gmail.com>
2018-08-28 00:09:57 +0800

    修改结构，开始前台页面开发

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,14 +2,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-	
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('Pages_Model');
+		$this->load->model('Article_Model');
+	}
+	public function index(){
+
+		$about_us = $this->Pages_Model->getInfoById(1);
+		$data['about_us'] = $about_us['description'];
+
+		
+
+		$this->load->view('home/index.html', $data);
 	}
 
-	public function hi()
-	{
-		echo 'hi~';
-	}
 }
